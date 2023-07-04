2ac688f60079cd66b7d3c28f96e40d2abf26b965
marina <mrnymda@gmail.com>
2023-07-05 00:14:04 +0900

    Topページ基本機能 Navbar, ユーザー認証

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const NavLinks = [
+  { href: '/', key: 'hairstyles', text: 'Hair style' },
+  { href: '/', key: 'nails', text: 'Nails' },
+  { href: '/', key: 'eyelashes', text: 'Eyelashes' },
+];
+
+export const categoryFilters = [
+  "Hair Styles",
+  "Nails",
+  "Eyelashes",
+  "Eyebrows",
+  "Makeups",
+]
+
--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -6,8 +6,10 @@ const User = g.model('User', {
   email: g.string().unique(),
   avatarUrl: g.url(),
   description: g.string().length({ min: 2, max: 1000 }).optional(),
-  InstagramUrl: g.url().optional(), 
+  InstagramUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
 })
 
 // @ts-ignore
@@ -19,6 +21,21 @@ const Project = g.model('Project', {
   category: g.string().search(),
   createdBy: g.relation(() => User),
 }).auth((rules) => {
+  // 他のuserは見るのみ
   rules.public().read()
+  // 本人のみ作成・削除・更新可能
   rules.private().create().delete().update()
+})
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret:  g.env('NEXTAUTH_SECRET')
+})
+
+export default config({
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private()
+  },
 })
